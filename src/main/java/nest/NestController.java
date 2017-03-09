82523a29f9ac61c6ecc883f8cb2ee27faff5c37f
Chris <Chris@hawking>
2017-03-09 14:17:15 +0000

    remove debug

--- src/main/java/nest/NestController.java
+++ src/main/java/nest/NestController.java
@@ -44,12 +44,10 @@ public class NestController extends AlexaController {
 	    	if ( token == null ) {
 	    		return new LinkAccountResponse();
 	    	}
-	    	System.out.println("token = "+token);
 	    	Request request = Request.Get(Constants.URL_NEST_FIREBASE);
 	    	request.addHeader("Authorization", "Bearer "+token);
 	    	
 	    	String response = request.execute().returnContent().asString();
-	    	System.out.println("nest-data: "+response);
 	    	double temperature = NestAPI.construct(response).getCurrentTemp();
 	    	
 	    	return endSessionResponse("Current temperature is "+temperature);
