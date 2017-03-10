131d77eeb9fb7b2a1527877bc64260c0cd2f7613
Chris <Chris@hawking>
2017-03-10 12:09:38 +0000

    removed not needed URLs

--- src/main/java/nest/Constants.java
+++ src/main/java/nest/Constants.java
@@ -1,14 +1,7 @@
 package nest;
 
 public interface Constants {
-
-    String PRODUCT_ID = "d0187803-8cdc-4517-8261-329f1d2d2a66";
-    String PRODUCT_SECRET = "Mp6o6Yzti2P29UNpNJM3Z3TDy";
     String REDIRECT_URL = "https://glacial-scrubland-57921.herokuapp.com/glacial-scrubland-57921/callback";
-
     String URL_NEST_FIREBASE = "https://developer-api.nest.com";
-  
-
-    String URL_AUTHORIZATION_FORMAT = "https://home.nest.com/login/oauth2?client_id=d0187803-8cdc-4517-8261-329f1d2d2a66&state=STATE";
-	
+    String URL_AUTHORIZATION_FORMAT = "https://home.nest.com/login/oauth2?client_id=$CLIENT_ID&state=STATE";
 }
