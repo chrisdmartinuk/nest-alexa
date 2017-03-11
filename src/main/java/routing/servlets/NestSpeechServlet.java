a11008cf6d8dcb0bd2ea58fddf12449a5a22a30a
Chris <Chris@hawking>
2017-03-11 10:37:34 +0000

    debug

--- src/main/java/routing/servlets/NestSpeechServlet.java
+++ src/main/java/routing/servlets/NestSpeechServlet.java
@@ -3,6 +3,7 @@ package routing.servlets;
 import java.io.IOException;
 import java.util.Enumeration;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,6 +16,10 @@ public class NestSpeechServlet extends SpeechletServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		Enumeration<String> iter = request.getHeaderNames();
+		System.out.println("cookies");
+		for ( Cookie c : request.getCookies() ) {
+			System.out.println(c.getName()+" "+c.getValue());
+		}
 		System.out.println("Header:");
 		while ( iter.hasMoreElements() ) {
 			String s = iter.nextElement();
